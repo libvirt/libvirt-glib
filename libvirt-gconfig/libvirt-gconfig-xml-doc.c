4bfbf93f4f5d454c5507602111d0796e65a67daf
Christophe Fergeau <cfergeau@redhat.com>
2011-11-02 18:21:07 +0100

    Add GVirConfigXmlDoc type
    This class derives from GObject and wraps an xmlDocPtr. It will be
    useful to refcount xmlDoc so that it can be shared between multiple
    GVirConfigObject instances.

---
+++ libvirt-gconfig/libvirt-gconfig-xml-doc.c
@@ -0,0 +1,135 @@
+/*
+ * libvirt-gobject-config-xml-node.c: libvirt glib integration
+ *
+ * Copyright (C) 2011 Red Hat
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ * Author: Christophe Fergeau <cfergeau@gmail.com>
+ */
+
+#include <config.h>
+
+#include <string.h>
+
+#include <libxml/tree.h>
+
+#include "libvirt-gconfig/libvirt-gconfig.h"
+#include "libvirt-gconfig/libvirt-gconfig-xml-doc.h"
+
+extern gboolean debugFlag;
+
+#define GVIR_CONFIG_XML_DOC_GET_PRIVATE(obj)                         \
+        (G_TYPE_INSTANCE_GET_PRIVATE((obj), GVIR_TYPE_CONFIG_XML_DOC, GVirConfigXmlDocPrivate))
+
+struct _GVirConfigXmlDocPrivate
+{
+    xmlDocPtr doc;
+};
+
+G_DEFINE_TYPE(GVirConfigXmlDoc, gvir_config_xml_doc, G_TYPE_OBJECT);
+
+enum {
+    PROP_0,
+    PROP_DOC
+};
+
+static void gvir_config_xml_doc_get_property(GObject *object,
+                                              guint prop_id,
+                                              GValue *value,
+                                              GParamSpec *pspec)
+{
+    GVirConfigXmlDoc *xml_doc = GVIR_CONFIG_XML_DOC(object);
+
+    switch (prop_id) {
+    case PROP_DOC:
+        g_value_set_pointer(value, xml_doc->priv->doc);
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+    }
+}
+
+static void gvir_config_xml_doc_set_property(GObject *object,
+                                              guint prop_id,
+                                              const GValue *value,
+                                              GParamSpec *pspec)
+{
+    GVirConfigXmlDoc *doc = GVIR_CONFIG_XML_DOC(object);
+
+    switch (prop_id) {
+    case PROP_DOC:
+        doc->priv->doc = g_value_get_pointer(value);
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+    }
+}
+
+static void gvir_config_xml_doc_finalize(GObject *object)
+{
+    GVirConfigXmlDoc *doc = GVIR_CONFIG_XML_DOC(object);
+    GVirConfigXmlDocPrivate *priv = doc->priv;
+
+    g_debug("Finalize GVirConfigXmlDoc=%p", doc);
+
+    if (priv->doc)
+        xmlFreeDoc(priv->doc);
+
+    G_OBJECT_CLASS(gvir_config_xml_doc_parent_class)->finalize(object);
+}
+
+static void gvir_config_xml_doc_class_init(GVirConfigXmlDocClass *klass)
+{
+    GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
+    g_type_class_add_private(klass, sizeof(GVirConfigXmlDocPrivate));
+
+    object_class->finalize = gvir_config_xml_doc_finalize;
+    object_class->get_property = gvir_config_xml_doc_get_property;
+    object_class->set_property = gvir_config_xml_doc_set_property;
+
+    g_object_class_install_property(object_class,
+                                    PROP_DOC,
+                                    g_param_spec_pointer("doc",
+                                                        "XML Doc",
+                                                        "The XML doc this config object corresponds to",
+                                                        G_PARAM_READWRITE |
+                                                        G_PARAM_CONSTRUCT_ONLY |
+                                                        G_PARAM_STATIC_STRINGS));
+}
+
+
+static void gvir_config_xml_doc_init(GVirConfigXmlDoc *xml_doc)
+{
+    GVirConfigXmlDocPrivate *priv;
+
+    g_debug("Init GVirConfigXmlDoc=%p", xml_doc);
+
+    priv = xml_doc->priv = GVIR_CONFIG_XML_DOC_GET_PRIVATE(xml_doc);
+
+    memset(priv, 0, sizeof(*priv));
+}
+
+
+GVirConfigXmlDoc *gvir_config_xml_doc_new(xmlDocPtr doc)
+{
+    if (doc == NULL) {
+        doc = xmlNewDoc((xmlChar *)"1.0");
+    }
+    return GVIR_CONFIG_XML_DOC(g_object_new(GVIR_TYPE_CONFIG_XML_DOC,
+                                             "doc", doc,
+                                             NULL));
+}
---
+++ libvirt-gconfig/libvirt-gconfig-xml-doc.h
@@ -0,0 +1,66 @@
+/*
+ * libvirt-gobject-xml-doc.h: libvirt gobject integration
+ *
+ * Copyright (C) 2011 Red Hat
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ * Author: Christophe Fergeau <cfergeau@gmail.com>
+ */
+
+#if !defined(__LIBVIRT_GCONFIG_H__) && !defined(LIBVIRT_GCONFIG_BUILD)
+#error "Only <libvirt-gconfig/libvirt-gconfig.h> can be included directly."
+#endif
+
+#ifndef __LIBVIRT_GCONFIG_XML_DOC_H__
+#define __LIBVIRT_GCONFIG_XML_DOC_H__
+
+G_BEGIN_DECLS
+
+#define GVIR_TYPE_CONFIG_XML_DOC            (gvir_config_xml_doc_get_type ())
+#define GVIR_CONFIG_XML_DOC(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GVIR_TYPE_CONFIG_XML_DOC, GVirConfigXmlDoc))
+#define GVIR_CONFIG_XML_DOC_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GVIR_TYPE_CONFIG_XML_DOC, GVirConfigXmlDocClass))
+#define GVIR_IS_CONFIG_XML_DOC(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GVIR_TYPE_CONFIG_XML_DOC))
+#define GVIR_IS_CONFIG_XML_DOC_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GVIR_TYPE_CONFIG_XML_DOC))
+#define GVIR_CONFIG_XML_DOC_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GVIR_TYPE_CONFIG_XML_DOC, GVirConfigXmlDocClass))
+
+typedef struct _GVirConfigXmlDoc GVirConfigXmlDoc;
+typedef struct _GVirConfigXmlDocPrivate GVirConfigXmlDocPrivate;
+typedef struct _GVirConfigXmlDocClass GVirConfigXmlDocClass;
+
+struct _GVirConfigXmlDoc
+{
+    GObject parent;
+
+    GVirConfigXmlDocPrivate *priv;
+
+    /* Do not add fields to this struct */
+};
+
+struct _GVirConfigXmlDocClass
+{
+    GObjectClass parent_class;
+
+    gpointer padding[20];
+};
+
+
+GType gvir_config_xml_doc_get_type(void);
+
+GVirConfigXmlDoc *gvir_config_xml_doc_new(xmlDocPtr doc);
+
+G_END_DECLS
+
+#endif /* __LIBVIRT_GCONFIG_XML_DOC_H__ */
