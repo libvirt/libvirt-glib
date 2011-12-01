afff1a9cd9639f8f9184de215d1887a92daaba3a
Daniel P. Berrange <berrange@redhat.com>
2011-12-01 12:16:54 +0000

    Remove indentation with TABs from autogen.sh & conn-test.py

--- examples/conn-test.py
+++ examples/conn-test.py
@@ -11,20 +11,20 @@ canc = Gio.Cancellable()
 
 def done(conn, result, data):
     try:
-	conn.open_finish(result)
+        conn.open_finish(result)
 
-	print("Opened " + conn.get_uri())
+        print("Opened " + conn.get_uri())
 
-	conn.fetch_domains(None)
-	print ("Fetched")
-	doms = conn.get_domains()
-	print ("Got " + str(doms))
+        conn.fetch_domains(None)
+        print ("Fetched")
+        doms = conn.get_domains()
+        print ("Got " + str(doms))
 
-	for d in doms:
-	    print ("One dom: " + str(d))
-	    print ("Name " + d.get_name())
-	    conf = d.get_config(0)
-	    print ("Conf " + str(conf))
+        for d in doms:
+            print ("One dom: " + str(d))
+            print ("Name " + d.get_name())
+            conf = d.get_config(0)
+            print ("Conf " + str(conf))
 
             try:
                 conf.validate()
@@ -33,12 +33,12 @@ def done(conn, result, data):
             except Exception, e:
                 print "Document is not valid according to %s: %s: %s" % (conf.get_schema(), str(e), str(type(e)))
 
-	    xml = conf.get_doc()
-	    print ("XML " + xml)
-	    print ("Info " + str(d.get_info().memory))
+            xml = conf.get_doc()
+            print ("XML " + xml)
+            print ("Info " + str(d.get_info().memory))
 
     finally:
-	Gtk.main_quit()
+        Gtk.main_quit()
 
 
 conn.open_async(canc, done, None)
