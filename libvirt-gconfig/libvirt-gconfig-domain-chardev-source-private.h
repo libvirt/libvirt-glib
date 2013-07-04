3cf2e242408d0a8a6058f14ab0f6ca9227269049
Daniel P. Berrange <berrange@redhat.com>
2013-07-04 11:53:08 +0100

    Add missing file from previous commit
    The commit 35a310c62a8bf704aceb3a5b3ecce36c11525914 forgot
    to add libvirt-gconfig/libvirt-gconfig-domain-chardev-source-private.h
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

---
+++ libvirt-gconfig/libvirt-gconfig-domain-chardev-source-private.h
@@ -0,0 +1,40 @@
+/*
+ * libvirt-gconfig-domain-chardev-source-private.h: libvirt domain chardev configuration
+ *
+ * Copyright (C) 2013 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Author: Daniel P. Berrange <berrange@redhat.com>
+ */
+
+#ifndef __LIBVIRT_GCONFIG_DOMAIN_CHARDEV_SOURCE_PRIVATE_H__
+#define __LIBVIRT_GCONFIG_DOMAIN_CHARDEV_SOURCE_PRIVATE_H__
+
+#include <libvirt-gconfig/libvirt-gconfig-xml-doc.h>
+
+G_BEGIN_DECLS
+
+GVirConfigDomainChardevSource *
+gvir_config_domain_chardev_source_new_from_tree(GVirConfigXmlDoc *doc,
+                                                xmlNodePtr tree);
+
+GVirConfigDomainChardevSource *
+gvir_config_domain_chardev_source_pty_new_from_tree(GVirConfigXmlDoc *doc,
+                                                    xmlNodePtr tree);
+
+G_END_DECLS
+
+#endif /* __LIBVIRT_GCONFIG_DOMAIN_CHARDEV_SOURCE_PRIVATE_H__ */
