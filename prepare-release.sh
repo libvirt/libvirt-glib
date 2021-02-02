b34f59c752cec0da7a4e89e959959911e04a9aba
Andrea Bolognani <abologna@redhat.com>
2021-02-02 16:48:24 +0100

    prepare-release: Fix indentation
    Some versions of cppi complain about this.
    
    Fixes: 626e042769ebc9455a44d76f84da6b72a697a6ac
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- prepare-release.sh
+++ prepare-release.sh
@@ -11,14 +11,14 @@ ninja -C build/native dist
 
 if [ -f /usr/bin/rpmbuild ]; then
   rpmbuild --nodeps \
-    --define "_sourcedir `pwd`/build/native/meson-dist" \
-    -ba --clean build/native/libvirt-glib.spec
+           --define "_sourcedir `pwd`/build/native/meson-dist" \
+           -ba --clean build/native/libvirt-glib.spec
 fi
 
 # Test mingw32 cross-compile
 if test -x /usr/bin/i686-w64-mingw32-gcc ; then
   meson --prefix="`pwd`/build/vroot" \
-	--cross-file=/usr/share/mingw/toolchain-mingw32.meson build/win32
+        --cross-file=/usr/share/mingw/toolchain-mingw32.meson build/win32
 
   ninja -C build/win32
 fi
@@ -26,7 +26,7 @@ fi
 # Test mingw64 cross-compile
 if test -x /usr/bin/x86_64-w64-mingw32-gcc ; then
   meson --prefix="`pwd`/build/vroot" \
-	--cross-file=/usr/share/mingw/toolchain-mingw64.meson build/win64
+        --cross-file=/usr/share/mingw/toolchain-mingw64.meson build/win64
 
   ninja -C build/win64
 fi
@@ -34,7 +34,7 @@ fi
 if test -x /usr/bin/i686-w64-mingw32-gcc && test -x /usr/bin/x86_64-w64-mingw32-gcc ; then
   if test -f /usr/bin/rpmbuild ; then
     rpmbuild --nodeps \
-      --define "_sourcedir `pwd`/build/native/meson-dist" \
-      -ba --clean build/native/mingw-libvirt-glib.spec
+             --define "_sourcedir `pwd`/build/native/meson-dist" \
+             -ba --clean build/native/mingw-libvirt-glib.spec
   fi
 fi
