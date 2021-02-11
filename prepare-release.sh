libvirt/libvirt-glib

commit 626e042769ebc9455a44d76f84da6b72a697a6ac

build: convert the release script over to using meson
Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

prepare-release.sh
@@ -3,79 +3,38 @@
 set -e
 set -v
 
-test -n "$1" && RESULTS=$1 || RESULTS=results.log
-
-# Make things clean.
-test -f Makefile && make -k distclean || :
-
 rm -rf build
-mkdir build
-cd build
 
-../autogen.sh --prefix=$HOME/builder \
-    --enable-gtk-doc
+meson --prefix="`pwd`/build/vroot" -Denable-gtk-doc=true build/native
 
-make
-make install
-
-# set -o pipefail is a bashism; this use of exec is the POSIX alternative
-exec 3>&1
-st=$(
-  exec 4>&1 >&3
-  { make check syntax-check 2>&1 3>&- 4>&-; echo $? >&4; } | tee "$RESULTS"
-)
-exec 3>&-
-test "$st" = 0
-
-rm -f *.tar.gz
-make dist
+ninja -C build/native dist
 
 if [ -f /usr/bin/rpmbuild ]; then
   rpmbuild --nodeps \
-     --define "_sourcedir `pwd`" \
-     -ba --clean libvirt-glib.spec
+    --define "_sourcedir `pwd`/build/native/meson-dist" \
+    -ba --clean build/native/libvirt-glib.spec
 fi
 
 # Test mingw32 cross-compile
 if test -x /usr/bin/i686-w64-mingw32-gcc ; then
-  make distclean
+  meson --prefix="`pwd`/build/vroot" \
+	--cross-file=/usr/share/mingw/toolchain-mingw32.meson build/win32
 
-  PKG_CONFIG_PATH="$HOME/builder/i686-w64-mingw32/sys-root/mingw/lib/pkgconfig" \
-  CC="i686-w64-mingw32-gcc" \
-  ../configure \
-    --build=$(uname -m)-pc-linux \
-    --host=i686-w64-mingw32 \
-    --prefix="$HOME/builder/i686-w64-mingw32/sys-root/mingw" \
-    --enable-werror \
-    --enable-introspection=no
-
-  make
-  make install
-
+  ninja -C build/win32
 fi
 
 # Test mingw64 cross-compile
 if test -x /usr/bin/x86_64-w64-mingw32-gcc ; then
-  make distclean
+  meson --prefix="`pwd`/build/vroot" \
+	--cross-file=/usr/share/mingw/toolchain-mingw64.meson build/win64
 
-  PKG_CONFIG_PATH="$HOME/builder/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig" \
-  CC="x86_64-w64-mingw32-gcc" \
-  ../configure \
-    --build=$(uname -m)-pc-linux \
-    --host=x86_64-w64-mingw32 \
-    --prefix="$HOME/builder/i686-w64-mingw32/sys-root/mingw" \
-    --enable-werror \
-    --enable-introspection=no
-
-  make
-  make install
-
+  ninja -C build/win64
 fi
 
 if test -x /usr/bin/i686-w64-mingw32-gcc && test -x /usr/bin/x86_64-w64-mingw32-gcc ; then
   if test -f /usr/bin/rpmbuild ; then
     rpmbuild --nodeps \
-       --define "_sourcedir `pwd`" \
-       -ba --clean mingw-libvirt-glib.spec
+      --define "_sourcedir `pwd`/build/native/meson-dist" \
+      -ba --clean build/native/mingw-libvirt-glib.spec
   fi
 fi

commit 24fe5f36ebc6441edfc82ee3aa484de6b48b3096

prepare-release: Fix Meson invocation
The option used to choose whether to enabled gtk-doc support
has been renamed.

Fixes: 626e042769ebc9455a44d76f84da6b72a697a6ac

Signed-off-by: Andrea Bolognani <abologna@redhat.com>

prepare-release.sh
@@ -5,7 +5,7 @@ set -v
 
 rm -rf build
 
-meson --prefix="`pwd`/build/vroot" -Denable-gtk-doc=true build/native
+meson --prefix="`pwd`/build/vroot" -Ddocs=true build/native
 
 ninja -C build/native dist
 